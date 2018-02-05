d93a6887f97b056c8210a779c65dbac9ee7c212f
Natsuki Mari <marinatsuki6295@gmail.com>
2018-02-06 03:02:11 +0700

    Turn this into Class

--- CMF-Editor/CMF Editor.xaml.cs
+++ CMF-Editor/CMF Editor.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using CMF_Editor.Classes;
 
 namespace CMF_Editor
 {
@@ -189,43 +190,6 @@ namespace CMF_Editor
         public int Icon { get; set; }
     }
 
-    public class SortAdorner : Adorner
-    {
-        private static Geometry ascGeometry =
-                Geometry.Parse("M 0 4 L 3.5 0 L 7 4 Z");
-
-        private static Geometry descGeometry =
-                Geometry.Parse("M 0 0 L 3.5 4 L 7 0 Z");
-
-        public ListSortDirection Direction { get; private set; }
-
-        public SortAdorner(UIElement element, ListSortDirection dir)
-                : base(element)
-        {
-            this.Direction = dir;
-        }
-
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            base.OnRender(drawingContext);
-
-            if (AdornedElement.RenderSize.Width < 20)
-                return;
-
-            TranslateTransform transform = new TranslateTransform
-                    (
-                            AdornedElement.RenderSize.Width - 15,
-                            (AdornedElement.RenderSize.Height - 5) / 2
-                    );
-            drawingContext.PushTransform(transform);
-
-            Geometry geometry = ascGeometry;
-            if (this.Direction == ListSortDirection.Descending)
-                geometry = descGeometry;
-            drawingContext.DrawGeometry(Brushes.Black, null, geometry);
-
-            drawingContext.Pop();
-        }
-    }
+    
 
 }
