c6d4990151e0a913b655a99eadf9a88cb23e4a2a
Dramiel Leayal <whitered12316@gmail.com>
2018-02-22 19:23:19 +0700

    BetterFolderBrowseDialog

---
+++ CMF-Editor/WinForms/Interop/helpers.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace CMF_Editor.WinForms.Interop
+{
+    internal static class Helpers
+    {
+        // TODO: Remove Helpers class, refactor
+        internal static Window GetDefaultOwnerWindow()
+        {
+            Window defaultWindow = null;
+
+            // TODO: Detect active window and change to that instead
+            if (Application.Current != null && Application.Current.MainWindow != null)
+            {
+                defaultWindow = Application.Current.MainWindow;
+            }
+            return defaultWindow;
+        }
+
+    }
+}
