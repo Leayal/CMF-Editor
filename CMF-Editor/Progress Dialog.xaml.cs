baadc84e8dbf6db4f6c9da5b9c76b5ae3b1f51bf
Natsuki Mari <marinatsuki6295@gmail.com>
2018-02-23 14:58:05 +0700

    Progress Dialog

---
+++ CMF-Editor/Progress Dialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CMF_Editor
+{
+    /// <summary>
+    /// Interaction logic for Progress_Dialog.xaml
+    /// </summary>
+    public partial class Progress_Dialog : Window
+    {
+        public Progress_Dialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
