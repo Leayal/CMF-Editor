951b1918e0ad82620e4b30e7c315b8ec94f29972
Natsuki Mari <marinatsuki6295@gmail.com>
2018-03-05 21:41:55 +0700

    Overwrite or Rename Window

---
+++ CMF-Editor/Overwrite or Rename.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CMF_Editor
+{
+    /// <summary>
+    /// Interaction logic for Overwrite_or_Rename.xaml
+    /// </summary>
+    public partial class Overwrite_or_Rename : Window
+    {
+        public Overwrite_or_Rename()
+        {
+            InitializeComponent();
+        }
+    }
+}
